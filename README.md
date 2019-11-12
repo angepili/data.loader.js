6256bafa32718fe76a64ae756307b6e373e1f386
Angel <angepili@icloud.com>
2019-11-12 22:41:29 +0100

    remove jQuery dependecy; General improves

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 # data.loader.js
 
+```html
+    <div id="DataLoaderContent"></div>
+    <button id="LoadMore">Carica altro</button>
+```
+
 ```javascript
-var dataLoader = new DataLoader(),
-    ENDPOINT = 'https://jsonplaceholder.typicode.com/posts';
-    dataLoader.loadContent({
-        endpoint : ENDPOINT,
-        show : 6, // items to show
-        data : {} // optional data send request
-        callback : foo
+    DataLoader({
+        endpoint : 'https://jsonplaceholder.typicode.com/posts', // Required,
+        show : 6, // Set number of items to show and offset; Optional, 6 by defaut
+        template :  `
+            <div class="item">
+                <h2>{id} - {title}</h2>
+                <p class="bio">{body}</p>
+            </div>
+            `, // Define the TPL to iterate, Use {variable} to replace automatically obj key; required
+        container: 'DataLoaderContent', // Set ID of container; Optional, use "DataLoaderContent" by default
+        loadMore: 'LoadMore' // Set ID of button; Optional, use "LoadMore" by default
     });
 ```
--- data.loader.js
+++ data.loader.js
@@ -1,6 +1,15 @@
-  var DataLoader = function(){
+var DataLoader = function(obj) {
        
         var DATA = [];
+
+        var Options = {
+            endpoint    : obj.endpoint ? obj.endpoint : '',
+            params      : obj.data ? Object.keys(obj.data).map(function(key) { return key + '=' + obj.data[key]; }).join('&') : '',
+            show        : obj.show ? obj.show : 10,
+            container   : obj.container ? obj.container : 'DataLoaderContent',
+            loadMore    : obj.loadMore ? obj.loadMore : 'LoadMore',
+            template    : obj.template
+        }
 
         var proprieties = {
             offset : 0,
@@ -43,46 +52,56 @@
             }
         }
 
-        var loadContent = function(obj){
+        Object.size = function(obj) {
+            var size = 0, key;
+            for (key in obj) {
+                if (obj.hasOwnProperty(key)) size++;
+            }
+            return size;
+        };
 
-            var endpoint = obj.endpoint,
-                callback = obj.callback,
-                data = obj.data,
-                show = obj.show;
-            
-            jQuery.ajax({
-                url: endpoint,
-                type: 'GET',
-                data : data,
-                success: function(data){
-                    jQuery('.block-loader').fadeOut();
-                    DATA = data;
-                    DATA.content = data.content;
-                    DATA.show = show;
-                    callback(data);
-
-                }
-            })
+        function replaceMe(template, data) {
+            const pattern = /{\s*(\w+?)\s*}/g;
+            return template.replace(pattern, (_, token) => data[token] || '');
         }
 
-        var getEvents = function(){
+        var Load = function(){
+            const { endpoint, params, show } = Options;
+            fetch(endpoint+params)
+                .then(response => {
+                    if (response.status !== 200) {
+                      console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                      return;
+                    }
+                
+                    response.json().then(data => {
+                        DATA = data;
+                        GetContent();
+                        LoadMore();
+                        // Filters();
+                    });
 
-            var container = jQuery('#content_container');
+                    
+                })
+        }
 
-                data = [];
+        var GetContent = ()=>{
 
-                if(proprieties.offset == 0) proprieties.offset = DATA.show;
+            var container = document.getElementById(Options.container);
+
+                if(proprieties.offset == 0) proprieties.offset = Options.show;
 
                 if(!('search' in proprieties) && Object.size(proprieties.filters) == 0){
-                    data = DATA.content.slice(0,proprieties.offset);
-                    total = DATA.info.total;
+                    tmp = DATA.slice(0,proprieties.offset);
+                    total = Object.size(DATA);
                 } else if('search' in proprieties) {
-                    if(proprieties.offset == 0) proprieties.offset = DATA.show;
-                    total = Object.size(DATA.content);
-                    data = total < DATA.show ? DATA.content : DATA.content.slice(0,proprieties.offset);
+                    if(proprieties.offset == 0) proprieties.offset = Options.show;
+                    total = Object.size(DATA);
+                    tmp = total < Options.show ? DATA : DATA.slice(0,proprieties.offset);
                 } else {
-                    if(proprieties.offset == 0) proprieties.offset = DATA.show;
-                    data = proprieties.result.slice(0,proprieties.offset);
+                    if(proprieties.offset == 0) proprieties.offset = Options.show;
+                    tmp = proprieties.result.slice(0,proprieties.offset);
                     total = Object.size(proprieties.result);
                 }
 
@@ -90,71 +109,72 @@
                 current = proprieties.offset;
 
                 if(total == 0){
-                    container.html('<div class="clearfix"><div class="alert alert-danger">Nessun evento per questa selezione di filtri</div></div>')
+                    container.innterHtml = '<div class="clearfix"><div class="alert alert-danger">Nessun contenuto per questa selezione di filtri</div></div>';
                 }
                 
-                var objData = {};
-                    objData.data = data;
-                var item = document.querySelector('#contentHbs').innerHTML,
-                    hbs = Handlebars.compile(item);
-                    container.html(hbs(objData));
+                container.innerHTML = '';
+                for(var i = 0; i < Object.size(tmp); i++){
+                    container.innerHTML += replaceMe(Options.template,tmp[i]);
+                }
 
         }
 
-        var loadMoreEvents = function(){
+        var LoadMore = function(){
 
-            jQuery(document).on('click','#load_more',function(e){
+            document.getElementById(Options.loadMore).addEventListener('click',(e)=>{
                 e.preventDefault();
-                proprieties.offset += 5;
-                getEvents();                
-            });
-    
+                proprieties.offset += Options.show;
+                GetContent();  
+            })
+
+        }
+
+        /**
+         * TODO: da riscrivere
+         */
+        /* 
+            var Filters = function(){
             var buttons = jQuery('button[data-filter]');
-                buttons.on('click',function(e){
-                    e.preventDefault();
-                    proprieties.offset = 0;
-                    var that = jQuery(this),
-                        container = that.parent(),
-                        key = that.data('type'),
-                        value = that.data('filter').toString();
-                        active = that.hasClass('active');
+            buttons.on('click',function(e){
+                e.preventDefault();
+                proprieties.offset = 0;
+                var that = jQuery(this),
+                    container = that.parent(),
+                    key = that.data('type'),
+                    value = that.data('filter').toString();
+                    active = that.hasClass('active');
+                    that.removeClass('active');
+
+                    container.find('button[data-type="'+key+'"]').removeClass('active');
+
+                    if('search' in proprieties){
+                        data = cache;
+                        delete proprieties.search;
+                    }
+
+                    if(!active){
+                        that.addClass('active');
+                        proprieties.setFilter(key,value);
+                        if(value == '*'){
+                            proprieties.removeFilter(key);
+                        }
+                    } else {
                         that.removeClass('active');
-
-                        container.find('button[data-type="'+key+'"]').removeClass('active');
-
-                        if('search' in proprieties){
-                            data = cache;
-                            delete proprieties.search;
+                        if(key in proprieties.filters){
+                            proprieties.removeFilter(key);
                         }
-    
-                        if(!active){
-                            that.addClass('active');
-                            proprieties.setFilter(key,value);
-                            if(value == '*'){
-                                proprieties.removeFilter(key);
-                            }
-                        } else {
-                            that.removeClass('active');
-                            if(key in proprieties.filters){
-                                proprieties.removeFilter(key);
-                            }
-                        }
-                        
-                        if(container.find('button[data-type].active').length == 0){
-                            container.find('button[data-type]:eq(0)').addClass('active');
-                        }
-    
-                        proprieties.applyFilters(DATA.content);
-                        getEvents();
-    
-                });
+                    }
+                    
+                    if(container.find('button[data-type].active').length == 0){
+                        container.find('button[data-type]:eq(0)').addClass('active');
+                    }
 
-        }
+                    proprieties.applyFilters(DATA);
+                    GetContent();
+            });
+        } */
 
-        return {
-            loadContent : loadContent,
-            loadMoreEvents : loadMoreEvents,
-            getEvents : getEvents
-        }
+
+        return Load();
 
 }
